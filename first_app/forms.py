381295c7b61c9a6eb6d3404d2bb556506cc95ce8
pro <pro@prodeMacBook-Pro-2.local>
2023-08-10 18:06:40 +0600

    date and time add

--- first_app/forms.py
+++ first_app/forms.py
@@ -1,19 +1,23 @@
 from django import forms
 
+# widgets == field to html input
+
 
 class contactForm(forms.Form):
     name = forms.CharField(
-        label="User Name:", initial="Tanver", help_text="Total length 70 char", required=False, disabled=True)
+        label="User Name:", help_text="Total length 70 char", required=False, disabled=False, widget=forms.Textarea(attrs={'id': 'text_area', 'placeholder': 'Enter your name'}))
     # file = forms.FileField()
 
-    # email = forms.EmailField(label="User Email")
+    email = forms.EmailField(label="User Email")
     # age = forms.IntegerField()
     # weight = forms.FloatField()
     # balance = forms.DecimalField()
-    # check = forms.BooleanField()
-    # birthday = forms.DateField()
-    # appointment = forms.DateTimeField()
-    # CHOICES = [('S', 'Small'), ('M', 'Medium'), ('L', 'Large')]
-    # size = forms.ChoiceField(choices=CHOICES)
-    # meal = [('p', 'pepperoni'), ('M', 'Mashroom'), ('B', 'Beef')]
-    # pizza = forms.MultipleChoiceField(choices=meal)
+    age = forms.CharField(widget=forms.NumberInput)
+    check = forms.BooleanField()
+    birthday = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    appointment = forms.DateTimeField(
+        widget=forms.DateInput(attrs={'type': 'datetime-local'}))
+    CHOICES = [('S', 'Small'), ('M', 'Medium'), ('L', 'Large')]
+    size = forms.ChoiceField(choices=CHOICES)
+    meal = [('p', 'pepperoni'), ('M', 'Mashroom'), ('B', 'Beef')]
+    pizza = forms.MultipleChoiceField(choices=meal)

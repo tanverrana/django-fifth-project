5b879d02a2194e83ab20735fc6430b42f14e06c5
pro <pro@prodeMacBook-Pro-2.local>
2023-08-10 10:42:24 +0600

    redirect form submission in about page

--- first_app/views.py
+++ first_app/views.py
@@ -8,9 +8,14 @@ def home(request):
 
 
 def about(request):
-    return render(request, './first_app/about.html')
+    if request.method == 'POST':
+        name = request.POST.get('username')
+        email = request.POST.get('email')
+        return render(request, './first_app/about.html', {'name': name, 'email': email})
+    else:
+        return render(request, './first_app/about.html', {'name': name, 'email': email})
 
 
 def submit_form(request):
-    print(request.POST)
+
     return render(request, './first_app/form.html')

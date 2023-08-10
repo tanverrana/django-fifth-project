df4cd21bc9faa20cbdf949a8405f573289bd2e6e
pro <pro@prodeMacBook-Pro-2.local>
2023-08-10 16:35:27 +0600

    django form field

--- first_app/forms.py
+++ first_app/forms.py
@@ -4,3 +4,9 @@ from django import forms
 class contactForm(forms.Form):
     name = forms.CharField(label="User Name")
     email = forms.EmailField(label="User Email")
+    age = forms.IntegerField()
+    weight = forms.FloatField()
+    balance = forms.DecimalField()
+    check = forms.BooleanField()
+    birthday = forms.DateField()
+    appointment = forms.DateTimeField()
